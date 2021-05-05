2205725b0a8c2e2fb3e92731d1635809c716116f
biwaskerung <biwas.kerung99@gmail.com>
2021-05-05 07:09:52 +0545

    logout code

--- hello/chalan/urls.py
+++ hello/chalan/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views
 
+from django.contrib.auth import views as auth_views
+from users import views as user_views
+
 from . import views
 
 urlpatterns = [
@@ -10,4 +13,10 @@ urlpatterns = [
     path('create/', views.create, name = 'create'),
     path('about/', views.about, name = 'about'),
     path('contact/', views.contact, name = 'contact'),
+
+    path('profile/', views.contact, name = 'profile'),
+
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name = 'logout'),
+
+
 ]
--- hello/hello/settings.py
+++ hello/hello/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chalan',
+    'users',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#for profile picture\
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'Static')  
 ]
