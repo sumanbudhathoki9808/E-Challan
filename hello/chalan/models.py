380d9a188efc3322bfd5966d7862a34835b1a169
Binayak Gc <binayakgc@gmail.com>
2021-05-06 12:27:19 +0545

    file added for create challan

--- hello/chalan/models.py
+++ hello/chalan/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class challan(models.Model):
+    names = models.CharField(max_length = 200)
+    phonenumber = models.IntegerField(max_length = 200)
+    place = models.CharField(max_length = 200)
+    licensenumber = models.IntegerField(max_length = 200)
+    vehiclenumber = models.CharField(max_length = 200)
+    vehicletype = models.CharField(max_length = 200)
+    creator = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return self.licensenumber
