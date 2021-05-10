323a4b65bc61a923b6f4e01b91877c14376d2b14
Binayak Gc <binayakgc@gmail.com>
2021-05-10 22:15:58 +0545

    print challan

--- echallan/home/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Challan(models.Model):
-
-    chalan_amount = models.IntegerField()
-    chalan_on_name = models.CharField(max_length = 100)
-    creator_officer = models.CharField(max_length = 100)
