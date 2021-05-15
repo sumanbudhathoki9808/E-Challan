d86490bc4edbb4a16272acd1bbed3989eed9e65e
Rabi KC <np03a190133@heraldcollege.edu.np>
2021-05-15 23:11:16 +0545

    changes in design and web updates

--- hello/chalan/urls.py
+++ hello/chalan/urls.py
@@ -4,9 +4,9 @@ from django.contrib.auth import views
 from . import views
 
 urlpatterns = [
-    path('', views.loginPage, name = 'loginPage'),
+    path('login/', views.loginPage, name = 'loginPage'),
     # path('register/', views.register, name = 'register'),
-    path('home/', views.home, name = 'home'),
+    path('', views.home, name = 'home'),
     path('create/', views.create, name = 'create'),
     path('about/', views.about, name = 'about'),
     path('contact/', views.contact, name = 'contact'),
--- hello/chalan/views.py
+++ hello/chalan/views.py
@@ -1,10 +1,5 @@
-# from django.shortcuts import render, redirect
-# # from .model import *
+
 from django.views.decorators.csrf import csrf_exempt
-# from django.contrib.auth.forms import UserCreationForm
-
-# Create your views here.
-
 from .form import CreateUserForm
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
@@ -15,6 +10,82 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import challan, dynamicAbout, contact
 from . import models
 from django.contrib.auth.decorators import login_required
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username = username, password = password)
+       
+        if user is not None:
+            login(request,user)
+            return redirect('/create')
+        else:
+            messages.error(request, 'Username or password is incorrect')
+
+    context = {}
+    return render(request,'login.html',context)
+
+def logouts(request):
+    logout(request)
+    
+    return redirect('/')
+
+# @login_required(login_url='/')
+def home(request):
+    return render(request, "home.html")
+
+@login_required(login_url='/')
+def create(request):
+    if request.method == "POST":
+        names = request.POST.get('names')
+        phonenumber = request.POST.get('phonenumber')
+        place = request.POST.get('place')
+        licensenumber = request.POST.get('licensenumber')
+        vehiclenumber = request.POST.get('vehiclenumber')
+        vehicletype = request.POST.get('vehicletype')
+        creator = request.POST.get('creator')
+
+        ins = models.challan(names = names, phonenumber = phonenumber,  place=place, licensenumber = licensenumber, vehiclenumber=vehiclenumber, vehicletype=vehicletype, creator=creator)
+        ins.save()
+
+    return render(request, "create.html")
+
+# @login_required(login_url='/')
+def about(request):
+
+    about = dynamicAbout.objects.all()
+
+
+    return render(request, "about.html", {"context" : about})
+
+# @login_required(login_url='/')
+def contact(request):
+
+    if request.method == "POST":
+        # challans = challan()
+        usernames = request.POST.get('usernames')
+        phonenumbers = request.POST.get('phonenumbers')
+        email = request.POST.get('email')
+        descs = request.POST.get('descs')
+
+        ins = models.contact(usernames = usernames, phonenumbers = phonenumbers,  email=email, descs = descs)
+        ins.save()
+
+    return render(request, "contact.html")
+
+@login_required(login_url='/')
+def viewchallan(request):
+
+    rec = challan.objects.filter(creator = request.user)
+
+    return render(request, "viewchallan.html", {"record" : rec })
+
+# from django.shortcuts import render, redirect
+# # from .model import *
+# from django.contrib.auth.forms import UserCreationForm
+# Create your views here.
 # Create your views here.
 # from .models import *
 # from .forms import CreateUserForm
@@ -43,75 +114,3 @@ from django.contrib.auth.decorators import login_required
 #     # context = {'form': forms}
 
 #     return render(request, "register.html", {'form': forms})
-
-def loginPage(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        user = authenticate(request, username = username, password = password)
-       
-        if user is not None:
-            login(request,user)
-            return redirect('/home')
-        else:
-            messages.info(request, 'Username or password is incorrect')
-
-    context = {}
-    return render(request,'login.html',context)
-
-def logouts(request):
-    logout(request)
-    
-    return redirect('/')
-
-@login_required(login_url='/')
-def home(request):
-    return render(request, "home.html")
-
-@login_required(login_url='/')
-def create(request):
-    if request.method == "POST":
-        # challans = challan()
-        names = request.POST.get('names')
-        phonenumber = request.POST.get('phonenumber')
-        place = request.POST.get('place')
-        licensenumber = request.POST.get('licensenumber')
-        vehiclenumber = request.POST.get('vehiclenumber')
-        vehicletype = request.POST.get('vehicletype')
-        creator = request.POST.get('creator')
-
-        ins = models.challan(names = names, phonenumber = phonenumber,  place=place, licensenumber = licensenumber, vehiclenumber=vehiclenumber, vehicletype=vehicletype, creator=creator)
-        ins.save()
-
-    return render(request, "create.html")
-
-@login_required(login_url='/')
-def about(request):
-
-    about = dynamicAbout.objects.all()
-
-
-    return render(request, "about.html", {"context" : about})
-
-@login_required(login_url='/')
-def contact(request):
-
-    if request.method == "POST":
-        # challans = challan()
-        usernames = request.POST.get('usernames')
-        phonenumbers = request.POST.get('phonenumbers')
-        email = request.POST.get('email')
-        descs = request.POST.get('descs')
-
-        ins = models.contact(usernames = usernames, phonenumbers = phonenumbers,  email=email, descs = descs)
-        ins.save()
-
-    return render(request, "contact.html")
-
-@login_required(login_url='/')
-def viewchallan(request):
-
-    rec = challan.objects.filter(creator = request.user)
-
-    return render(request, "viewchallan.html", {"record" : rec })
